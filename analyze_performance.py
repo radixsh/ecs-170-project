8131ba699b953064f1345951c0d64f16300c1d64
Radish <53844660+radixsh@users.noreply.github.com>
2024-11-04 18:34:18 -0800

    Compute avg loss over 10 different runs

--- analyze_performance.py
+++ analyze_performance.py
@@ -9,8 +9,8 @@ from train import MyDataset
 from generate_data import generate_data
 from env import DISTRIBUTION_TYPES, SAMPLE_SIZE, TEST_SIZE, MODEL, LOSS_FN, DEVICE
 
-logger = logging.getLogger("meta")
-logger.setLevel(logging.DEBUG)
+logger = logging.getLogger("analyze_performance")
+logger.setLevel(logging.INFO)
 
 def test(dataloader, model, loss_fn, device):
     size = len(dataloader.dataset)
@@ -35,21 +35,27 @@ def test(dataloader, model, loss_fn, device):
 
     test_loss /= num_batches
     logger.info(f"Avg loss: {test_loss:>8f}")
+    return test_loss
 
 def main():
-    # Load existing model, and run it for TEST_SIZE tests
     weights_path = 'model_weights.pth'
     MODEL.load_state_dict(torch.load(weights_path))
 
-    raw_test_data = generate_data(count=TEST_SIZE, sample_size=SAMPLE_SIZE)
-    test_samples = np.array([elem[0] for elem in raw_test_data])
-    test_labels = np.array([elem[1] for elem in raw_test_data])
-    test_dataset = MyDataset(test_samples, test_labels)
-    test_dataloader = DataLoader(test_dataset)
+    LOSS_FN = nn.L1Loss()
+    avg_loss = 0
+    tests = 20
+    for _ in range(tests):
+        raw_test_data = generate_data(count=TEST_SIZE, sample_size=SAMPLE_SIZE)
+        test_samples = np.array([elem[0] for elem in raw_test_data])
+        test_labels = np.array([elem[1] for elem in raw_test_data])
+        test_dataset = MyDataset(test_samples, test_labels)
+        test_dataloader = DataLoader(test_dataset)
 
-    # Run it on a slightly modified test() function in this file (the only
-    # difference is that it gives more verbose logging)
-    test(test_dataloader, MODEL, LOSS_FN, DEVICE)
+        # Run it on a slightly modified test() function in this file (the only
+        # difference is that it gives more verbose logging)
+        avg_loss += test(test_dataloader, MODEL, LOSS_FN, DEVICE)
+    avg_loss /= tests
+    logger.info(f"Avg loss over {tests} tests: {avg_loss}")
 
 if __name__ == "__main__":
     main()
