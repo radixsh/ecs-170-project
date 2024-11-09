3f985e6cf35b2adbf113dd5aad3edd9ca0029f9b
luukjanssen9 <luukjanssen9@outlook.com>
2024-11-08 17:37:43 -0800

    added dist_var_matrix to loss_fn.py, since that is the only place it is being used. Can be moved back if it will be used in other files

--- env.py
+++ env.py
@@ -53,26 +53,3 @@ DEVICE = (
 
 LOSS_FN = CustomLoss()  # Custom loss function defined in loss_fn.py
 LEARNING_RATE = 1e-3    # Learning rate, for optimizer
-
-# "Support fail penalty": the amount to add to loss when the network
-# TOTALLY guessues wrong for distribution type. This is going away
-# This will eventually go away
-SFP = 0.99  
-
-# Manually calculated: (total variation distance) / sqrt(2) to normalize between 0 and 1
-# See: https://www.desmos.com/calculator/8h3zthas2q
-# Indexed by DISTRIBUTION_TYPES
-# Each entry is the non-similarity of two distributions
-# If the distributions have different support, use SFP
-# Symmetric about the diagonal (which is all 0s)
-DIST_VAR_MATRIX = [
-    [0  ,SFP,SFP,SFP,SFP,SFP,SFP,SFP,SFP], # beta [0,1]
-    [SFP,0  ,SFP,SFP,SFP,0.113506763819,0.353553390593,0.217681125289,0.113036008855], # gamma R+
-    [SFP,SFP,0  ,0.122458776456,0.0933338655318,SFP,0.0968575030474,SFP,SFP], # gumbel R
-    [SFP,SFP,0.122458776456,0  ,0.065553960094,SFP,0.0999157699984,SFP,SFP], # laplace R
-    [SFP,SFP,0.0933338655318,0.065553960094,0  ,SFP,0.0384284266337,SFP,SFP], # logistic R
-    [SFP,0.113506763819,SFP,SFP,SFP,0  ,SFP,0.171801196924,0.0298717548468], # lognormal R+
-    [SFP,0.353553390593,0.0968575030474,0.0999157699984,0.0384284266337,SFP,0  ,SFP,SFP], # normal R
-    [SFP,0.217681125289,SFP,SFP,SFP,0.171801196924,SFP,0  ,0.19627978404], # rayleigh R+
-    [SFP,0.113036008855,SFP,SFP,SFP,0.0298717548468,SFP,0.19627978404,0  ]  # wald R+
-]
--- loss_fn.py
+++ loss_fn.py
@@ -7,6 +7,28 @@ import torch.nn as nn
 class CustomLoss(nn.Module):
     def __init__(self):
         super(CustomLoss, self).__init__()
+        # "Support fail penalty": the amount to add to loss when the network
+        # TOTALLY guessues wrong for distribution type. This is going away
+        # This will eventually go away
+        SFP = 0.99  
+
+        # Manually calculated: (total variation distance) / sqrt(2) to normalize between 0 and 1
+        # See: https://www.desmos.com/calculator/8h3zthas2q
+        # Indexed by DISTRIBUTION_TYPES
+        # Each entry is the non-similarity of two distributions
+        # If the distributions have different support, use SFP
+        # Symmetric about the diagonal (which is all 0s)
+        self.dist_var_matrix = [
+            [0  ,SFP,SFP,SFP,SFP,SFP,SFP,SFP,SFP], # beta [0,1]
+            [SFP,0  ,SFP,SFP,SFP,0.113506763819,0.353553390593,0.217681125289,0.113036008855], # gamma R+
+            [SFP,SFP,0  ,0.122458776456,0.0933338655318,SFP,0.0968575030474,SFP,SFP], # gumbel R
+            [SFP,SFP,0.122458776456,0  ,0.065553960094,SFP,0.0999157699984,SFP,SFP], # laplace R
+            [SFP,SFP,0.0933338655318,0.065553960094,0  ,SFP,0.0384284266337,SFP,SFP], # logistic R
+            [SFP,0.113506763819,SFP,SFP,SFP,0  ,SFP,0.171801196924,0.0298717548468], # lognormal R+
+            [SFP,0.353553390593,0.0968575030474,0.0999157699984,0.0384284266337,SFP,0  ,SFP,SFP], # normal R
+            [SFP,0.217681125289,SFP,SFP,SFP,0.171801196924,SFP,0  ,0.19627978404], # rayleigh R+
+            [SFP,0.113036008855,SFP,SFP,SFP,0.0298717548468,SFP,0.19627978404,0  ]  # wald R+
+        ]
 
     # Returns a dist type as an index in the DISTRIBUTION_TYPES
     def dist_type(self, vec, dim_count):
@@ -73,7 +95,7 @@ class CustomLoss(nn.Module):
                 dist_vec_guess[dim_count*(env.NUM_DISTS+2) - 1]
             )
             # Search the lookup table, get the number of the dists in the list first
-            dist_loss += env.DIST_VAR_MATRIX[self.dist_type(dist_vec_actual,dim_count)][self.dist_type(dist_vec_actual,dim_count)]
+            dist_loss += self.dist_var_matrix[self.dist_type(dist_vec_actual,dim_count)][self.dist_type(dist_vec_actual,dim_count)]
             dim_count += 1
         # average of mean_loss and stddev_loss, times  1/(1-dist loss/dimensions)
         # dist_loss of 0 means loss just average of mean_loss and stddev_loss
