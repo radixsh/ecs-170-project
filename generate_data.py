6dc0ed3d5f85f2cc63ba9619bf161bef26c03603
Radish <53844660+radixsh@users.noreply.github.com>
2024-11-15 15:43:31 -0800

    Write script to generate data and save it out

--- generate_data.py
+++ generate_data.py
@@ -1,5 +1,12 @@
 import logging
+import os
+import numpy as np
+import torch
+from torch.utils.data import DataLoader
+
 from distributions import DISTRIBUTION_FUNCTIONS
+from env import CONFIG
+from dataset import MyDataset
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -11,3 +18,27 @@ def generate_data(count, sample_size):
             points, labels = dist_func(sample_size)
             data.append((points, labels))
     return data
+
+# If running this file directly as a script, then generate some training
+# examples and save them to a file for later use
+if __name__ == "__main__":
+    # Make sure the 'data' directory exists
+    data_directory = 'data'
+    os.makedirs(data_directory, exist_ok=True)
+
+    # https://pytorch.org/docs/stable/generated/torch.save.html
+    raw_train_data = generate_data(count=CONFIG['TRAINING_SIZE'],
+                                   sample_size=CONFIG['SAMPLE_SIZE'])
+    train_samples = np.array([elem[0] for elem in raw_train_data])
+    train_labels = np.array([elem[1] for elem in raw_train_data])
+    train_dataset = MyDataset(train_samples, train_labels)
+    train_dataloader = DataLoader(train_dataset, batch_size=CONFIG['BATCH_SIZE'])
+    torch.save(train_dataloader, 'data/train_dataloader')
+
+    raw_test_data = generate_data(count=CONFIG['TRAINING_SIZE'],
+                                  sample_size=CONFIG['SAMPLE_SIZE'])
+    test_samples = np.array([elem[0] for elem in raw_test_data])
+    test_labels = np.array([elem[1] for elem in raw_test_data])
+    test_dataset = MyDataset(test_samples, test_labels)
+    test_dataloader = DataLoader(test_dataset, batch_size=CONFIG['BATCH_SIZE'])
+    torch.save(test_dataloader, 'data/test_dataloader')
