626d805da58b1a003a518a5d762cf2431167a718
Radish <53844660+radixsh@users.noreply.github.com>
2024-11-18 19:16:32 -0800

    Consolidate files, move hyperparam values to env.py - Combine train_multiple and pipeline - Combine generate_data and distributions - Move the list of hyperparameter values to env.py (previously, it was hidden in   train_multiple.py)

--- generate_data.py
+++ generate_data.py
@@ -2,16 +2,157 @@ import logging
 import os
 import numpy as np
 import torch
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader
 import time
+import math
+import mpmath
 
-from distributions import DISTRIBUTION_FUNCTIONS
-from env import CONFIG
+from env import CONFIG, MEAN_SCALE
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 console_handler = logging.StreamHandler()
 logger.addHandler(console_handler)
+
+### SEED THE RNG
+rng = np.random.default_rng()
+
+### HELPERS
+# Returns a uniformly random value in (-1, 1)
+def generate_mean():
+    sign = rng.choice([-1, 1])
+    return rng.uniform() * sign * MEAN_SCALE
+# Returns a random positive value according to the function x*exp(-x) (mean=1)
+def generate_stddev():
+    return rng.gamma(2, scale=1)
+# Returns a uniformly random value in (0, 1]
+def generate_pos_mean():
+    return (1 - rng.uniform()) * MEAN_SCALE
+
+## SUPPORT = R
+
+def normal(sample_size):
+    mean = generate_mean()
+    stddev = generate_stddev()
+    labels = [0,0,0,0,0,0,1,0,0,mean,stddev]
+    sample_data = rng.normal(mean, stddev, sample_size)
+    return (sample_data, labels)
+
+def gumbel(sample_size):
+    mean = generate_mean()
+    stddev = generate_stddev()
+    labels = [0,0,1,0,0,0,0,0,0,mean,stddev]
+
+    # stddev^2 = (pi^2 beta^2) / 6
+    # beta = stddev * sqrt(6) / pi
+    beta = stddev * math.sqrt(6) / math.pi
+
+    # euler is the Euler-Mascheroni constant
+    # mean = mu + beta * euler
+    # mu = mean - beta * euler
+    mu = mean - beta * float(mpmath.euler)
+
+    sample_data = rng.gumbel(mu, beta, sample_size)
+    return (sample_data, labels)
+
+def laplace(sample_size):
+    mean = generate_mean()
+    stddev = generate_stddev()
+    labels = [0,0,0,1,0,0,0,0,0,mean,stddev]
+
+    # stddev^2 = 2b^2
+    # b = sqrt(2)*stddev
+    b = stddev * math.sqrt(2)
+    sample_data = rng.laplace(mean, b, sample_size)
+    return (sample_data, labels)
+
+def logistic(sample_size):
+    mean = generate_mean()
+    stddev = generate_stddev()
+    labels = [0,0,0,0,1,0,0,0,0,mean,stddev]
+
+    # stddev^2 = pi^2 s^2 * 1/3
+    # s = sqrt(3)/pi * stddev
+    s = stddev * math.sqrt(3) / math.pi
+    sample_data = rng.logistic(mean, s, sample_size)
+    return (sample_data, labels)
+
+
+## SUPPORT >= 0
+
+def lognormal(sample_size):
+    mean = generate_pos_mean()
+    stddev = generate_stddev()
+    labels = [0,0,0,0,0,1,0,0,0,mean,stddev]
+
+    # mean = exp(mu + sigma^2/2)
+    # stddev^2 = (exp(sigma^2) - 1) exp(2 mu - sigma^2)
+    # sigma = sqrt(ln(1 + (mean/stddev)^2))
+    # mu = ln(mean / sqrt(1 + (mean/stddev)^2))
+    sigma = math.sqrt(math.log(1 + (mean / stddev) ** 2))
+    mu = math.log(mean / math.sqrt(1 + (mean / stddev) ** 2))
+    sample_data = rng.lognormal(mu, sigma, sample_size)
+    return (sample_data, labels)
+
+def rayleigh(sample_size):
+    mean = generate_pos_mean()
+    stddev = generate_stddev()
+    labels = [0,0,0,0,0,0,0,1,0,mean,stddev]
+
+    # mean = sigma sqrt(pi/2)
+    # sigma = mean * sqrt(2/pi)
+    # One parameter, which must be positive
+    sigma = mean * math.sqrt(2 / math.pi)
+    sample_data = rng.rayleigh(sigma, sample_size)
+    return (sample_data, labels)
+
+## SUPPORT > 0
+# Non-positive mean is illegal for these distributions
+
+def beta(sample_size):
+    # Need a mean in (0, 1) and a stddev in (0, mean - mean^2)
+    # Possibly write this differently
+    mean = (generate_mean() / MEAN_SCALE + 1) / 2
+    stddev = ((generate_mean() / MEAN_SCALE + 1) / 2) * (mean - mean ** 2)
+    labels = [1,0,0,0,0,0,0,0,0,mean,stddev]
+
+    a = math.sqrt((mean ** 2 - mean ** 3) / stddev - mean)
+    b = (a / mean) - a
+    sample_data = rng.beta(a, b, sample_size)
+    return (sample_data, labels)
+
+def gamma(sample_size):
+    mean = generate_pos_mean()
+    stddev = generate_stddev()
+    labels = [0,1,0,0,0,0,0,0,0,mean,stddev]
+
+    k = (mean / stddev) ** 2
+    theta = (stddev ** 2) / mean
+    sample_data = rng.gamma(k, theta, sample_size)
+    return (sample_data, labels)
+
+def wald(sample_size):
+    mean = generate_pos_mean()
+    stddev = generate_stddev()
+    labels = [0,0,0,0,0,0,0,0,1,mean,stddev]
+
+    # stddev^2 = mean^3 / lam
+    # lam = mean^3 / stddev^2
+    lam = (mean ** 3) / (stddev ** 2)
+    sample_data = rng.wald(mean, lam, sample_size)
+    return (sample_data, labels)
+
+DISTRIBUTION_FUNCTIONS = {
+    "beta": beta,
+    "gamma": gamma,
+    "gumbel": gumbel,
+    "laplace": laplace,
+    "logistic": logistic,
+    "lognormal": lognormal,
+    "normal": normal,
+    "rayleigh": rayleigh,
+    "wald": wald,
+}
 
 class MyDataset(Dataset):
     def __init__(self, data, labels):
@@ -53,6 +194,40 @@ def make_dataset(filename, examples_count):
                 f"(BATCH_SIZE={CONFIG['BATCH_SIZE']})")
 
     return dataset
+
+def get_dataloader(config, filename=None, examples_count=-1):
+    try:
+        dataset = torch.load(filename)
+
+        # If examples_count is passed in, then adhere to it. Otherwise,
+        # examples_count is default value, indicating dataset can have any size
+        acceptable_count = (len(dataset) == examples_count \
+                            or examples_count == -1)
+
+        dataset_sample_size = len(dataset.__getitem__(0)[0])
+        acceptable_sample_size = (config['SAMPLE_SIZE'] == dataset_sample_size)
+
+        if not isinstance(dataset, Dataset):
+            raise Exception(f'Could not read dataset from {filename}')
+        if not acceptable_count:
+            raise Exception(f"Incorrect dataset size: {len(dataset)}")
+        if not acceptable_sample_size:
+            raise Exception(f"Incorrect sample size: model expects "
+                            f"{config['SAMPLE_SIZE']} points as input, but "
+                            f"this dataset would give {dataset_sample_size}")
+
+        logger.info(f"Using dataset from {filename} (size: {len(dataset)})")
+
+    except Exception as e:
+        logger.info(e)
+        logger.info(f'Generating fresh data...')
+        dataset = make_dataset(filename, examples_count)
+
+    # If no filename is passed in, the file does not exist, or the file's
+    # contents do not represent a DataLoader as expected, then generate some
+    # new data, and write it out to the given filename
+    dataloader = DataLoader(dataset, batch_size=config['BATCH_SIZE'])
+    return dataloader
 
 # If running this file directly as a script, then generate some training
 # examples and save them to a file for later use
--- train_multiple.py
+++ train_multiple.py
@@ -2,23 +2,77 @@ import logging
 import time
 import torch
 import os
+from torch.utils.data import Dataset
 
-from env import NUM_DIMENSIONS, DEVICE, CONFIG, HYPERPARAMETER
+from env import NUM_DIMENSIONS, DEVICE, CONFIG, HYPERPARAMETER, VALUES
 from build_model import build_model
-from pipeline import pipeline
-from distributions import DISTRIBUTION_FUNCTIONS
+from custom_functions import CustomLoss
+from generate_data import make_dataset, get_dataloader, DISTRIBUTION_FUNCTIONS
 
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 console_handler = logging.StreamHandler()
 logger.addHandler(console_handler)
+
+# TODO: Increase batch size (I think it's currently 1)
+# Batch size 1: gradient calculations might be noisy :(
+# Big batch size: potentially not enough compute :(
+# Medium batch size: good for implicit regularization :)
+def train_model(dataloader, model, loss_function, optimizer, device):
+    model.train()
+    size = len(dataloader.dataset)  # For debug logs
+
+    for batch, (X, y) in enumerate(dataloader):
+        X, y = X.to(device).float(), y.to(device).float()
+
+        pred = model(X)             # Forward pass
+        loss = loss_function(pred, y)     # Compute loss (prediction error)
+        loss.backward()             # Backpropagation
+        optimizer.step()
+        optimizer.zero_grad()
+
+        # if batch % 100 == 0:
+        loss_value = loss.item()
+        current = batch * dataloader.batch_size + len(X)#(batch + 1) * len(X)
+        logger.debug(f"Loss after batch {batch}:\t"
+                     f"{loss_value:>7f}  [{current:>5d}/{size:>5d}]")
+
+def pipeline(model, config):
+    start = time.time()
+
+    torch.manual_seed(42)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(42)
+
+    # Optimizer can't be in the config dict because it depends on model params
+    optimizer = torch.optim.SGD(model.parameters(),
+                                lr=config['LEARNING_RATE'], foreach=True)
+    # optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE, foreach=True)
+    # optimizer = torch.optim.Adamw(model.parameters(), lr=LEARNING_RATE, foreach=True)
+    # optimizer = torch.optim.Adagrad(model.parameters(), lr=LEARNING_RATE, foreach=True)
+
+    # Loss function can't be in config dict because custom_loss_function.py
+    # depends on config dict to build the loss function
+    loss_function = CustomLoss()
+
+    train_dataloader = get_dataloader(config, 'data/train_dataset',
+                                      examples_count=config['TRAINING_SIZE'])
+
+    logger.info(f"Training with {HYPERPARAMETER} = "
+                f"{config[HYPERPARAMETER]}...")
+
+    for epoch in range(config['EPOCHS']):
+        logger.debug(f"\nEpoch {epoch + 1}\n-------------------------------")
+        train_model(train_dataloader, model, loss_function, optimizer, DEVICE)
+
+    end = time.time()
+    logger.info(f"Finished training in {end - start:.2f} seconds")
+
+    return model.state_dict()
 
 # Goal: Train multiple models, and save each one
 def main():
     start = time.time()
-
-    hyperparams = [1e1, 1e2, 1e3, 1e4, 1e5]#, 1e6]
-    hyperparams = [int(i) for i in hyperparams]
 
     # Make sure the 'data' directory exists
     data_directory = 'data'
@@ -30,14 +84,14 @@ def main():
 
     # Filenames for various models
     dests = []
-    for i in hyperparams:
+    for i in VALUES:
         label = HYPERPARAMETER.lower().replace(' ', '_')
         dests.append(f'{models_directory}/weights_{label}_{i}.pth')
 
     # Train one model per training_size
-    for i, hyperparam in enumerate(hyperparams):
+    for i, value in enumerate(VALUES):
         # Update TRAINING_SIZE in the dict from env.py
-        CONFIG[HYPERPARAMETER] = hyperparam
+        CONFIG[HYPERPARAMETER] = value 
 
         # Initialize a new neural net
         input_size = CONFIG['SAMPLE_SIZE'] * NUM_DIMENSIONS
@@ -45,7 +99,7 @@ def main():
         model = build_model(input_size, output_size).to(DEVICE)
 
         # Train the model anew, and save the resulting model's weights out
-        logger.debug(f"Training model with {HYPERPARAMETER}={hyperparam}...")
+        logger.debug(f"Training model with {HYPERPARAMETER}={value}...")
         train_start = time.time()
 
         model_weights = pipeline(model, CONFIG)
